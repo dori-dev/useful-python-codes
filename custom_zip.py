8f3d062366e26b1a02236caffafabc929a440d40
Mohammad Dori <mr.dori.dev@gmail.com>
2021-11-29 19:29:44 +0330

    use custom_zip function

--- custom_zip.py
+++ custom_zip.py
@@ -22,3 +22,8 @@ def custom_zip(*iterables):
             result.append(element)
 
         yield tuple(result)
+
+numbers = [1, 2, 3]
+letters = ("a", "b", "c")
+strings = "xyz"
+print(*custom_zip(numbers, letters, strings))
