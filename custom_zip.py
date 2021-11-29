642539f1a7de28423c523341d55dc9a17d91b8c2
Mohammad Dori <mr.dori.dev@gmail.com>
2021-11-29 19:27:00 +0330

    add custom_zip function

--- custom_zip.py
+++ custom_zip.py
@@ -1,2 +1,18 @@
 """Custom Python Zip Function
 """
+
+
+def custom_zip(*iterables):
+    sentinel = object()
+    iterators = [iter(iterable) for iterable in iterables]
+
+    while iterators:
+        result = []
+
+        for iterator in iterators:
+            element = next(iterator, sentinel)
+            if element is sentinel:
+                return
+            result.append(element)
+
+        yield tuple(result)
